520e4087d6ba619cb50ce42f8c453ee5aaa1a467
Romecodes <olisaokoyej@gmail.com>
2022-09-08 19:36:24 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -is the entry point for the program
+ *
+ * Return:zero if no error, Non-zero value if error.
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of an int: %ld byte(S)\n", sizeof(int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
