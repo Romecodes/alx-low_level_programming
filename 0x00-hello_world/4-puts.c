d79778721af77a35dd7d6c498d4a6cdcc28a607f
Romecodes <olisaokoyej@gmail.com>
2022-09-08 18:33:57 -0700

    task4x

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main-Serves as the entry point for the program
- *
- * Return: By default returns zero if no erros otherwise returns non-zero value.
+ *i
+ * Return: By default return zero if no error otherwise return non-zero value.
  */
 int main(void)
 {
