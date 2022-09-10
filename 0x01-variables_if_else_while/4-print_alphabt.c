cd06b8c68f478d4bec5708787082cbc7ad8823b4
Romecodes <olisaokoyej@gmail.com>
2022-09-10 13:57:16 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.>
+/**
+ * main- Prints the program in lowercase
+ * followed by a new line
+ * Return: 0 when no errors occured,
+ * otherwise returns a positive integer
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		if (a != 'e' && a != 'q')
+			putchar(a);
+	putchar('\n');
+	return (0);
+}
