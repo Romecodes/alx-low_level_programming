491719345611da35eabe5b546bbd1d07dfea9069
Romecodes <olisaokoyej@gmail.com>
2022-09-10 11:51:57 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print out the alphabets in lowercase
+ * Return: 0 if no errors encounteredd,
+ * ele a positive integer is returned.
+ * */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar('\n');
+	}
+	putchar('\n');
+	retun (0);
+}
