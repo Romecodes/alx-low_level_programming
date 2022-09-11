38da8b5813ac01912d0c71e424a5377245038ca1
Romecodes <olisaokoyej@gmail.com>
2022-09-11 16:19:49 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- This program prints all possible combinations of
+ * single digit numbers; i.e
+ * 0, 1, 2, 3, 4, 5, 6, 7, 8, 9.
+ * Return: 0 if no errors encountered. else returns a +ve integer.
+ */
+int main(void)
+{
+	int n;
+
+	for (n ='0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
