b953e2edbc2780796b9b5c99f9a964fd90ff4611
Romecodes <olisaokoyej@gmail.com>
2022-09-12 13:09:23 -0700

    task 6 y

---
+++ 0x00-hello_world/daph.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print  "Programming is like building a multilingual puzzle
+ * Return 0: Always success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
