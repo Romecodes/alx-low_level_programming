0ec4db0775b9d808417faf917ab47aa18ec23801
Romecodes <olisaokoyej@gmail.com>
2022-09-14 16:40:59 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "mian.h"
+/**
+ *main - print _putchar followed by a new line.
+ *Return: 0 for a success value.
+ */
+int main(void)
+{
+	char und = 95;
+	char p = 112;
+	char u = 117;
+	char t = 116;
+	char c = 99;
+	char h = 104;
+	char a = 97;
+	char r = 114;
+
+	_putchar(und);
+       	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+       	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A Readme on functions, nested loops
