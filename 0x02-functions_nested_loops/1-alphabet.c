23566d4a971d5106f369329a15b76bf0e8aa1f22
Romecodes <olisaokoyej@gmail.com>
2022-09-14 17:09:38 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,15 @@
-#incclude "main.h
+#incclude "main.h"
+/**
+ * Print_alphaber - uses void _putchar to print out the alphabet.
+ * twice in 2 lines.
+ *
+ * Return: Void on success.
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
