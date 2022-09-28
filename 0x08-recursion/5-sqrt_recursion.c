10d75923145c6d3de8de60e893c9178e981d3d9e
Romecodes <olisaokoyej@gmail.com>
2022-09-28 12:52:43 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,3 +22,17 @@ int helper(int i, int n)
 	}
 	return (0);
 }
+/**
+ *_sqrt_recursion - returns square root
+ *@n: integer to return
+ *Return: returns int of squareroot
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (helper(i, n) == n && n != 1)
+		return (-1);
+	return (helper(i, n));
+}
