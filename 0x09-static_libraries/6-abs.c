d855d4f836de310cfc7b2f69e14d00ec008d9cbf
Romecodes <olisaokoyej@gmail.com>
2022-10-02 15:10:03 -0700

    task 0

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - This function computes the absolute value of an integer
+ * @a: Is the int argument for the _abs function.
+ *
+ * Return: 0 on success.
+ */
+int _abs(int a)
+{
+	return (abs(a));
+}
