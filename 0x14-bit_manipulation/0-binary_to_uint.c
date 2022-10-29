0b9c5cbfca0ae7b805566f2ce98c2c26789b12ae
Romecodes <olisaokoyej@gmail.com>
2022-10-29 10:35:18 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,16 +15,20 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
+		for (len = 0; b[len];)
+			len++;
 
-	for (len -= 1; len >= 0; len--)
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+		for (len -= 1; len >= 0; len--)
+			
+		{
+			if (b[len] != '0' && b[len] != '1')
 
+				return (0);
+
+		
 			num += (b[len] - '0') * mult;
 			mult *= 2;
-	}
-	return (num);
+		}
+
+		return (num);
 }
